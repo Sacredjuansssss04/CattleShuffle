6bac9740feedc8dd11ffc9b00406d05ee3d4b472
Sacredjuansssss04 <juanesteban04012007@gmail.com>
2025-10-26 23:14:50 -0500

    Inicio

---
+++ CattleShuffle/src/main/java/com/mycompany/cattleshuffle/CattleShuffle.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cattleshuffle;
+
+/**
+ *
+ * @author Sacred Juanss
+ */
+public class CattleShuffle {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
