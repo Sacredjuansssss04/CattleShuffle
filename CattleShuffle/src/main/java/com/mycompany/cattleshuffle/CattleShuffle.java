718a05eb57ebdb2179c522619dc5d14227e9e1ac
Sacredjuansssss04 <juanesteban04012007@gmail.com>
2025-10-26 23:15:47 -0500

    removiendo lo innecesario

--- CattleShuffle/src/main/java/com/mycompany/cattleshuffle/CattleShuffle.java
+++ CattleShuffle/src/main/java/com/mycompany/cattleshuffle/CattleShuffle.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.cattleshuffle;
 
-/**
- *
- * @author Sacred Juanss
- */
 public class CattleShuffle {
 
     public static void main(String[] args) {
